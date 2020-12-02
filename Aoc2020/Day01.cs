9e6626d69e04d2974613ffef60aa90c5dafa65e1
Jon Fuller <jcfuller@sep.com>
2020-12-02 07:35:01 -0500

    day 2, part 1

--- Aoc2020/Day01.cs
+++ Aoc2020/Day01.cs
@@ -1,12 +1,58 @@
 ﻿using System.IO;
 using System.Linq;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 using CliFx;
 using CliFx.Attributes;
 using MoreLinq;
+using static System.Console;
 
 namespace Aoc2020
 {
+    [Command(name: "day-02")]
+    public class Day02 : ICommand
+    {
+        public async ValueTask ExecuteAsync(IConsole console)
+        {
+            var validSamplePasswords = File
+               .ReadLines("input/02-sample")
+               .Select(PasswordEntry.Parse)
+               .Count(pe => pe.IsValid);
+
+            var part1 = File
+               .ReadLines("input/02-input")
+               .Select(PasswordEntry.Parse)
+               .Count(pe => pe.IsValid);
+
+            WriteLine($"Valid sample passwords: {validSamplePasswords}");
+            WriteLine($"Part 1: {part1}");
+        }
+
+        public record PasswordEntry(int Min, int Max, char Letter, string Password)
+        {
+            public static PasswordEntry Parse(string line)
+            {
+                var pattern = new Regex("^(?<min>\\d*)-(?<max>\\d*) (?<letter>[a-z]): (?<password>[a-z]*)$", RegexOptions.Compiled);
+                var match = pattern.Match(line);
+
+                return new PasswordEntry(
+                    int.Parse(match.Groups["min"].Value),
+                    int.Parse(match.Groups["max"].Value),
+                    match.Groups["letter"].Value.First(),
+                    match.Groups["password"].Value);
+
+            }
+            public bool IsValid
+            {
+                get
+                {
+                    var actual = Password.Count(c => c == Letter);
+                    return Min <= actual && actual <= Max;
+                }
+            }
+        }
+    }
+
     [Command(name: "day-01")]
     public class Day01 : ICommand
     {
