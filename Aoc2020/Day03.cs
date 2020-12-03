1719876bfa68847e1b0b0b9fb6a97ceb6d0728db
Jon Fuller <jcfuller@sep.com>
2020-12-03 08:26:12 -0500

    day 3, part 2

--- Aoc2020/Day03.cs
+++ Aoc2020/Day03.cs
@@ -13,37 +13,48 @@ namespace Aoc2020
     {
         public async ValueTask ExecuteAsync(IConsole console)
         {
-            console.Output.WriteLine(CountTrees("input/03-sample", (x: 3, y: 1)));
-            console.Output.WriteLine(CountTrees("input/03-input", (x: 3, y: 1)));
+            var sampleMap = Map.Load("input/03-sample");
+            var inputMap = Map.Load("input/03-input");
+            var slopes = new[]
+            {
+                (x: 1, y: 1),
+                (x: 3, y: 1),
+                (x: 5, y: 1),
+                (x: 7, y: 1),
+                (x: 1, y: 2)
+            };
 
-            static int CountTrees(string inputFile, (int x, int y) slope) => Map
-               .Load(inputFile)
+            console.Output.WriteLine(CountTrees(sampleMap, (x: 3, y: 1)));
+            console.Output.WriteLine(CountTrees(inputMap, (x: 3, y: 1)));
+            console.Output.WriteLine(SlopeProduct(sampleMap, slopes));
+            console.Output.WriteLine(SlopeProduct(inputMap, slopes));
+
+            static int CountTrees(Map map, (int x, int y) slope) => map
                .TraverseSlope(slope)
                .Count(c => c.IsTree);
+
+            long SlopeProduct(Map map, IEnumerable<(int x, int y)> slopes) => slopes
+               .Select(s => CountTrees(map, s))
+               .Pipe(x => console.Output.WriteLine(x))
+               .Aggregate(seed: (long)1, (seed, count) => seed * count);
         }
     }
 
     public record Cell(bool IsTree);
     public record Map(IEnumerable<IEnumerable<Cell>> Lines)
     {
-        public IEnumerable<Cell> TraverseSlope((int x, int y) slope)
-        {
-            return Lines
-               .TakeEvery(slope.y)
-               .Aggregate(seed: (currentX: 0, prev: Enumerable.Empty<Cell>()),
-                (acc, row) => (
-                    currentX: acc.currentX + slope.x,
-                            prev: acc.prev.Concat(new []{row.ElementAt(acc.currentX) })),
-                resultSelector: x => x.prev);
-        }
-        public static Map Load(string file)
-        {
-            var lines = File
-               .ReadLines(file)
-               .Select(row => row.Select(c => new Cell(c == '#')))
-               .Select(row => row.Repeat());
+        public IEnumerable<Cell> TraverseSlope((int x, int y) slope) => Lines
+           .TakeEvery(slope.y)
+           .Aggregate(seed: (currentX: 0, prev: Enumerable.Empty<Cell>()),
+            (acc, row) => (
+                currentX: acc.currentX + slope.x,
+                prev: acc.prev.Concat(new []{row.ElementAt(acc.currentX) })),
+            resultSelector: x => x.prev);
 
-            return new Map(lines);
-        }
+        public static Map Load(string file) => File
+           .ReadLines(file)
+           .Select(row => row.Select(c => new Cell(c == '#')))
+           .Select(row => row.Repeat())
+           .Then(_ => new Map(_));
     }
 }
---
+++ Aoc2020/Extensions.cs
@@ -0,0 +1,9 @@
+﻿using System;
+
+namespace Aoc2020
+{
+    public static class Extensions
+    {
+        public static TResult Then<TInput, TResult>(this TInput target, Func<TInput, TResult> f) => f(target);
+    }
+}
