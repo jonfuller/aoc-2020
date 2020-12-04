dfa8b97ffdb7beb95a2d1eec5092f332182b479f
Jon Fuller <jcfuller@sep.com>
2020-12-04 07:38:28 -0500

    day4, part 1

---
+++ Aoc2020/Day04.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using CliFx;
+using CliFx.Attributes;
+
+namespace Aoc2020
+{
+    [Command(name: "day-04")]
+    public class Day04 : ICommand
+    {
+        public async ValueTask ExecuteAsync(IConsole console)
+        {
+            console.Output.WriteLine(LoadPassports("input/04-sample").Count(p => p.ValidPart1));
+            console.Output.WriteLine(LoadPassports("input/04-input").Count(p => p.ValidPart1));
+        }
+
+        private static IEnumerable<Passport> LoadPassports(string filename) => File
+           .ReadLines(filename)
+           .Aggregate(
+                new List<string>() {""},
+                (acc, line) =>
+                {
+                    if (string.IsNullOrWhiteSpace(line))
+                        acc.Add("");
+                    acc[Index.FromEnd(1)] += " " + line;
+
+                    return acc;
+                })
+           .Select(ParsePassport);
+
+        private static Passport ParsePassport(string line)
+        {
+            return new Passport(line
+               .Split(' ', StringSplitOptions.RemoveEmptyEntries | StringSplitOptions.TrimEntries)
+               .Select(pair => pair.Split(':'))
+               .ToDictionary(pair => pair[0], pair => pair[1]));
+        }
+    }
+
+    public record Passport(Dictionary<string, string> Pairs)
+    {
+        private readonly IEnumerable<string> _requiredKeys = new[]
+        {
+            "byr",
+            "iyr",
+            "eyr",
+            "hgt",
+            "hcl",
+            "ecl",
+            "pid",
+        };
+
+        public bool ValidPart1 => _requiredKeys.All(Pairs.ContainsKey);
+    }
+}
