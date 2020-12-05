002f9af0ef993f953accea7e2b6b266eecd75dc7
Jon Fuller <jcfuller@sep.com>
2020-12-05 07:46:56 -0500

    day 5

---
+++ Aoc2020/Day05.cs
@@ -0,0 +1,59 @@
+﻿using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using CliFx;
+using CliFx.Attributes;
+using MoreLinq;
+
+namespace Aoc2020
+{
+    [Command(name: "day-05")]
+    public class Day05 : ICommand
+    {
+        public async ValueTask ExecuteAsync(IConsole console)
+        {
+            var samples = new[]
+            {
+                "FBFBBFFRLR",
+                "BFFFBBFRRR",
+                "FFFBBBFRRR",
+                "BBFFBBFRLL"
+            };
+
+            samples
+               .Select(s => new BoardingPass(s))
+               .ForEach(x => console.Output.WriteLine(x));
+
+            var passes = File.ReadLines("input/05-input")
+               .Select(s => new BoardingPass(s))
+               .OrderBy(p => p.SeatId)
+               .ToList();
+
+            console.Output.WriteLine(passes.MaxBy(b => b.SeatId).Single());
+
+            passes
+               .Lead(1, passes.First(),
+                    (a, b) => (diff: b.SeatId-a.SeatId, id: b.SeatId-1))
+               .First(x => x.diff != 1)
+               .Then(x => console.Output.WriteLine(x));
+        }
+    }
+
+    public record BoardingPass(string Encoded)
+    {
+        private const int NumRows = 128;
+        private const int NumCols = 8;
+
+        private int Row => Encoded.Take(7)
+           .Aggregate(Enumerable.Range(0, NumRows),
+                (acc, letter) => letter == 'F' ? acc.FrontHalf() : acc.BackHalf(),
+                acc => acc.Single());
+
+        private int Column => Encoded.Skip(7)
+           .Aggregate(Enumerable.Range(0, NumCols),
+                (acc, letter) => letter == 'L' ? acc.FrontHalf() : acc.BackHalf(),
+                acc => acc.Single());
+
+        public int SeatId => (Row * 8) + Column;
+    }
+}
--- Aoc2020/Extensions.cs
+++ Aoc2020/Extensions.cs
@@ -1,9 +1,24 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace Aoc2020
 {
     public static class Extensions
     {
         public static TResult Then<TInput, TResult>(this TInput target, Func<TInput, TResult> f) => f(target);
+        public static void Then<TInput>(this TInput target, Action<TInput> f) => f(target);
+
+        public static IEnumerable<T> FrontHalf<T>(this IEnumerable<T> target)
+        {
+            var listed = target.ToList();
+            return listed.GetRange(0, listed.Count / 2);
+        }
+
+        public static IEnumerable<T> BackHalf<T>(this IEnumerable<T> target)
+        {
+            var listed = target.ToList();
+            return listed.Skip(listed.Count / 2);
+        }
     }
 }
