dc39376fec410aaa872567a5edfbc6e576929bdb
Jon Fuller <fullerjc@gmail.com>
2020-12-06 08:38:30 -0500

    day 6, part 2

--- Aoc2020/Day06.cs
+++ Aoc2020/Day06.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
@@ -13,22 +14,41 @@ namespace Aoc2020
     {
         public async ValueTask ExecuteAsync(IConsole console)
         {
-            console.Output.WriteLine(CountUniqueAnswers("input/06-sample"));
-            console.Output.WriteLine(CountUniqueAnswers("input/06-input"));
+            var groupsSample = LoadGroups("input/06-sample");
+            var groupsInput = LoadGroups("input/06-input");
+
+            console.Output.WriteLine(CountUniqueAnswers(groupsSample));
+            console.Output.WriteLine(CountUniqueAnswers(groupsInput));
+            console.Output.WriteLine(CountUnanimous(groupsSample));
+            console.Output.WriteLine(CountUnanimous(groupsInput));
         }
 
-        int CountUniqueAnswers(string filename)
-        {
-            return File.ReadAllLines(filename)
-                .Split(string.IsNullOrWhiteSpace)
-                .Select(s => s.ToDelimitedString(string.Empty).Trim())
-                .Select(answers => answers.Distinct())
-                .Select(distinctAnswers => distinctAnswers.Count())
-                .Pipe(x =>
-                {
-                    Console.WriteLine(x);
-                })
-                .Sum();
-        }
+        int CountUniqueAnswers(IEnumerable<Group> groups) => groups
+            .Select(g => g.NumDistinctYes)
+            .Sum();
+
+        int CountUnanimous(IEnumerable<Group> groups) => groups
+            .Select(g => g.NumUnanimousYes)
+            .Sum();
+
+        IEnumerable<Group> LoadGroups(string filename) => File.ReadAllLines(filename)
+            .Split(string.IsNullOrWhiteSpace)
+            .Select(x => new Group(x));
+    }
+
+    public record Group(IEnumerable<string> Answers)
+    {
+        public int NumDistinctYes => DistinctYes.Count();
+        public int NumUnanimousYes => UnanimousYes.Count();
+
+        IEnumerable<char> DistinctYes => Answers
+            .ToDelimitedString(string.Empty)
+            .Trim()
+            .Distinct();
+
+        public IEnumerable<char> UnanimousYes => Enumerable
+            .Range(0, 26)
+            .Select(i => (char)('a'+i))
+            .Where(c => Answers.All(a => a.Contains(c)));
     }
 }
