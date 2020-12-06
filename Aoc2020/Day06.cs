d6a7327387355ef8ab2ddb1b2eeba3ded147cd6e
Jon Fuller <fullerjc@gmail.com>
2020-12-06 08:13:38 -0500

    day 6, part1

---
+++ Aoc2020/Day06.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using CliFx;
+using CliFx.Attributes;
+using MoreLinq;
+
+namespace Aoc2020
+{
+    [Command(name: "day-06")]
+    public class Day06 : ICommand
+    {
+        public async ValueTask ExecuteAsync(IConsole console)
+        {
+            console.Output.WriteLine(CountUniqueAnswers("input/06-sample"));
+            console.Output.WriteLine(CountUniqueAnswers("input/06-input"));
+        }
+
+        int CountUniqueAnswers(string filename)
+        {
+            return File.ReadAllLines(filename)
+                .Split(string.IsNullOrWhiteSpace)
+                .Select(s => s.ToDelimitedString(string.Empty).Trim())
+                .Select(answers => answers.Distinct())
+                .Select(distinctAnswers => distinctAnswers.Count())
+                .Pipe(x =>
+                {
+                    Console.WriteLine(x);
+                })
+                .Sum();
+        }
+    }
+}
