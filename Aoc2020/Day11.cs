a6357cb94ccb9be957bb74803034b47b41288af5
Jon Fuller <jcfuller@sep.com>
2020-12-12 07:51:58 -0500

    day 11, another version of part 1, partial part 2

--- Aoc2020/Day11.cs
+++ Aoc2020/Day11.cs
@@ -2,7 +2,6 @@
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
-using System.Text;
 using System.Threading.Tasks;
 using CliFx;
 using CliFx.Attributes;
@@ -16,14 +15,15 @@ namespace Aoc2020
         {
             var sampleLayout = LoadLayout("input/11-sample");
             var inputLayout = LoadLayout("input/11-input");
-            console.Output.WriteLine(SettleLayout(sampleLayout).NumOccupied);
-            console.Output.WriteLine(SettleLayout(inputLayout).NumOccupied);
+            console.Output.WriteLine(SettleLayout(sampleLayout, l => l.DoRoundPart1()).NumOccupied);
+            console.Output.WriteLine(SettleLayout(inputLayout, l => l.DoRoundPart1()).NumOccupied);
+            console.Output.WriteLine(SettleLayout(sampleLayout, l => l.DoRoundPart2()).NumOccupied);
         }
 
-        static Layout SettleLayout(Layout currentLayout)
+        static Layout SettleLayout(Layout currentLayout, Func<Layout, Layout> round)
         {
             Layout newLayout;
-            while (currentLayout.NumOccupied != (newLayout = currentLayout.DoRoundPart1()).NumOccupied)
+            while (currentLayout.NumOccupied != (newLayout = round(currentLayout)).NumOccupied)
             {
                 currentLayout = newLayout;
             }
@@ -35,43 +35,67 @@ namespace Aoc2020
             return File
                .ReadAllLines(filename)
                .Then(LoadSeats)
-               .Then(s => new Layout(s));
+               .Then(s => new Layout{Seats = s.ToList()});
 
-            Seat[,] LoadSeats(string[] lines)
+            static IEnumerable<Seat> LoadSeats(string[] lines)
             {
-                var seats = new Seat[lines[0].Length, lines.Length];
-
-                for (var i = 0; i < lines.Length; i++)
-                for (var j = 0; j < lines[i].Length; j++)
-                    seats[j, i] = new Seat(Seat.GetState(lines[i][j]));
-
-                return seats;
+                for (var y = 0; y < lines.Length; y++)
+                for (var x = 0; x < lines[y].Length; x++)
+                    yield return new Seat(Seat.GetState(lines[y][x]), x, y);
             }
         }
     }
 
-    public record Layout(Seat[,] Seats)
+    public record Layout
     {
-        private static readonly (int, int)[] NeighborDiffs = new[] { (-1, -1), (-1, 0), (-1, 1), (0, 1), (1, 1), (1, 0), (1, -1), (0, -1) };
+        private static readonly (int, int)[] NeighborDiffs = new[]
+            {(-1, -1), (-1, 0), (-1, 1), (0, 1), (1, 1), (1, 0), (1, -1), (0, -1)};
 
-        public Layout DoRoundPart1()
+        private readonly int _maxX;
+        private readonly int _maxY;
+        private readonly IList<Seat> _seats;
+        private readonly Dictionary<Seat, IList<Seat>> _neighborCache;
+        private readonly Dictionary<Seat, IList<Seat>> _nearestCache;
+
+        public IList<Seat> Seats
         {
-            var newSeats = new Seat[Seats.GetLength(0), Seats.GetLength(1)];
-            for (var i = 0; i < Seats.GetLength(0); i++)
-            for (var j = 0; j < Seats.GetLength(1); j++)
-                newSeats[i, j] = new Seat(NextState1((Seats[i, j].State, NumOccupiedNeighbors1(i, j))));
-
-            return new Layout(newSeats);
+            get => _seats;
+            init
+            {
+                _seats = value;
+                _maxX = _seats.Max(s => s.X);
+                _maxY = _seats.Max(s => s.Y);
+                var xyLookup = _seats
+                   .GroupBy(s => s.X, s => s)
+                   .ToDictionary(
+                        grp => grp.Key,
+                        grp => grp.OrderBy(i => i.Y).ToArray())
+                   .Select(x => x.Value)
+                   .ToArray();
+                
+                _neighborCache = _seats.ToDictionary(
+                    s => s,
+                    s => NeighborsFor(s, xyLookup));
+                _nearestCache = _seats.ToDictionary(
+                    s => s,
+                    s => NearestFor(s, xyLookup));
+            }
         }
-        public Layout DoRoundPart2()
+
+        public Layout DoRoundPart1() => this with
         {
-            var newSeats = new Seat[Seats.GetLength(0), Seats.GetLength(1)];
-            for (var i = 0; i < Seats.GetLength(0); i++)
-            for (var j = 0; j < Seats.GetLength(1); j++)
-                newSeats[i, j] = new Seat(NextState2((Seats[i, j].State, NumOccupiedNeighbors2(i, j))));
-
-            return new Layout(newSeats);
-        }
+            Seats = Seats.Select(s => s with
+            {
+                State = NextState1((s.State, NumOccupiedNeighbors1(s)))
+            }).ToList()
+        };
+        public Layout DoRoundPart2() => this with
+        {
+            Seats = Seats.Select(s => s with
+            {
+                State = NextState2((s.State, NumOccupiedNeighbors2(s)))
+            }).ToList()
+        };
 
         static Seat.SeatState NextState1((Seat.SeatState current, int numOccupiedNeighbors) x) => x switch
         {
@@ -79,7 +103,6 @@ namespace Aoc2020
             (Seat.SeatState.Occupied, numOccupiedNeighbors: >= 4) => Seat.SeatState.Empty,
             _ => x.current
         };
-        
         static Seat.SeatState NextState2((Seat.SeatState current, int numOccupiedNeighbors) x) => x switch
         {
             (Seat.SeatState.Empty, numOccupiedNeighbors: 0) => Seat.SeatState.Occupied,
@@ -87,45 +110,51 @@ namespace Aoc2020
             _ => x.current
         };
 
-        IEnumerable<(int i, int j, Seat seat)> All
+        public int NumOccupied => Seats.Count(seat => seat.State == Seat.SeatState.Occupied);
+
+        int NumOccupiedNeighbors1(Seat seat) => _neighborCache[seat].Count(x => x.State == Seat.SeatState.Occupied);
+        int NumOccupiedNeighbors2(Seat seat) => _nearestCache[seat].Count(x => x.State == Seat.SeatState.Occupied);
+
+        IList<Seat> NearestFor(Seat seat, Seat[][] xyLookup)
         {
-            get
+            return null;
+        }
+
+        IList<Seat> NeighborsFor(Seat seat, Seat[][] xyLookup)
+        {
+            return _().ToList();
+
+            IEnumerable<Seat> _()
             {
-                for (var i = 0; i < Seats.GetLength(0); i++)
-                for (var j = 0; j < Seats.GetLength(1); j++)
-                    yield return (i, j, Seats[i, j]);
+                foreach (var (x, y) in NeighborDiffs)
+                {
+                    var (newX, newY) = (x: seat.X + x, y: seat.Y + y);
+
+                    if (newX < 0) continue;
+                    if (newX > _maxX) continue;
+
+                    if (newY < 0) continue;
+                    if (newY > _maxY) continue;
+
+                    yield return xyLookup[newX][newY];
+                }
             }
         }
 
-        public int NumOccupied => All
-           .Select(x => x.seat.State)
-           .Count(x => x == Seat.SeatState.Occupied);
-
-        int NumOccupiedNeighbors1(int i, int j) => NeighborsFor(i, j)
-           .Count(n => n.State == Seat.SeatState.Occupied);
-        int NumOccupiedNeighbors2(int i, int j) => NeighborsFor(i, j)
-           .Count(n => n.State == Seat.SeatState.Occupied);
-
-        IEnumerable<Seat> NeighborsFor(int i, int j) => NeighborDiffs
-           .Select((d) => (i + d.Item1, j + d.Item2))
-           .Where(d => d.Item1 >= 0 && d.Item1 < Seats.GetLength(0))
-           .Where(d => d.Item2 >= 0 && d.Item2 < Seats.GetLength(1))
-           .Select(p => Seats[p.Item1, p.Item2]);
-
-        public override string ToString()
-        {
-            var builder = new StringBuilder();
-            for (var i = 0; i < Seats.GetLength(0); i++)
-            {
-                for (var j = 0; j < Seats.GetLength(1); j++)
-                    builder.Append(Seats[i, j].StateChar);
-                builder.Append(Environment.NewLine);
-            }
-            return builder.ToString();
-        }
+        //public override string ToString()
+        //{
+        //    var builder = new StringBuilder();
+        //    for (var i = 0; i < Seats.GetLength(0); i++)
+        //    {
+        //        for (var j = 0; j < Seats.GetLength(1); j++)
+        //            builder.Append(Seats[i, j].StateChar);
+        //        builder.Append(Environment.NewLine);
+        //    }
+        //    return builder.ToString();
+        //}
     }
     
-    public record Seat(Seat.SeatState State)
+    public record Seat(Seat.SeatState State, int X, int Y)
     {
         public enum SeatState { Occupied, Empty, Floor }
 
